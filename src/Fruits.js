3fe23b152666a477dc39007bf4f3ec6f612d467f
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-10-31 23:14:21 -0500

    delete functionality added

--- src/Fruits.js
+++ src/Fruits.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import NewFruit from './createForm.js';
 
 class Fruits extends Component {
-
+	constructor(){
+    super();
+    this.state = {
+      fruits: ["pear", "apple", "strawberry", "yuzu"]
+    	}
+  	}
+    deleteItem = (event) => {
+    	for (let i = 0; i < this.props.fruits.length; i++){
+    		if (event.currentTarget.parentNode.innerHTML.indexOf(this.props.fruits[i]) !== -1){
+    			this.props.fruits.splice(i, 1);
+    			const state = this.props;
+    			this.setState(state);
+    		} else {
+    			;
+    		}
+    	}
+    }
 	render() {
 		console.log(this.props)
 		const listFruits = this.props.fruits.map((item, i) => {
-			return <li key={i} onClick={this.props.crossOut}>{item}</li>
+			return <div key={i} listid={i}><li key={i} onClick={this.props.crossOut}>{item}</li><button onClick={this.deleteItem}>Delete</button></div>
 		})
 		return (
 			<div>
--- src/SrcList.js
+++ src/SrcList.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class List extends Component {
+	constructor(){
+    super();
+    this.state = {
+      list: ['Trey', 'Page', 'Mike', 'Fish'],
+    	}
+  	}
+    deleteItem = (event) => {
+    	for (let i = 0; i < this.props.list.length; i++){
+    		if (event.currentTarget.parentNode.innerHTML.indexOf(this.props.list[i]) !== -1){
+    			this.props.list.splice(i, 1);
+    			const state = this.props;
+    			this.setState(state);
+    		} else {
+    			;
+    		}
+    	}
+    }
 	render() {
 
 		const listItems = this.props.list.map((item, i) => {
-			return <li key={i} onClick={this.props.crossOut}>{item}</li>
+			return <div key={i}><li key={i} onClick={this.props.crossOut}>{item}</li><button onClick={this.deleteItem}>Delete</button></div>
 		})
 
 
